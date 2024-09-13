1102a37a2e5c82836d42fa9376f24c26542e9aaf
Chandan <chandanbhanopa@gmail.com>
2024-09-13 12:16:04 +0530

    Update grand total on mini cart

--- Plugin/AddDiscountPlugin.php
+++ Plugin/AddDiscountPlugin.php
@@ -4,14 +4,23 @@ namespace Chandan\DiscountOnMiniCart\Plugin;
 
 use Magento\Checkout\CustomerData\Cart;
 use Magento\Checkout\Model\Cart as MyCart;
+use Magento\Framework\Pricing\Helper\Data;
+use Magento\Tax\Model\Calculation as TaxCalculation;
 
 
 class AddDiscountPlugin {
 
 	protected $cart;
 
-	public function __construct(MyCart $cart){
+	protected $priceHelper;
+
+	protected $taxCalculation;
+
+	public function __construct(MyCart $cart, Data $priceHelper, TaxCalculation $taxCalculation){
 		$this->cart = $cart;
+		$this->priceHelper = $priceHelper;
+		$this->taxCalculation = $taxCalculation;
+
 	}
 
 
@@ -19,7 +28,46 @@ class AddDiscountPlugin {
     {
 		$data = $result;
 		$quote = $this->cart->getQuote();
-		$data['discount_amount'] = $quote->getSubtotal() - $quote->getSubtotalWithDiscount();  
+
+
+		/******/
+		$address = $quote->getShippingAddress();
+		$taxRateRequest = $this->taxCalculation->getRateRequest(
+			$address,
+			null,
+			null,
+			$quote->getStore() // Store object
+		);
+
+		$productTaxClassId = $quote->getItems()[0]->getProduct()->getTaxClassId();
+		$taxRate = $this->taxCalculation->getRate($taxRateRequest->setProductClassId($productTaxClassId));
+
+		$totalTax = 0;
+		foreach ($quote->getAllItems() as $item) {
+			$rowTotal = $item->getRowTotal(); 
+			$itemTax = $rowTotal * ($taxRate / 100); 
+			$totalTax += $itemTax;
+		}
+
+		if ($quote->getShippingAddress()->getShippingAmount()) {
+			$shippingAmount = $quote->getShippingAddress()->getShippingAmount();
+			$shippingTax = $shippingAmount * ($taxRate / 100); // Assuming same tax rate for shipping
+			$totalTax += $shippingTax;
+		}
+
+		$grandTotalWithTax = $quote->getGrandTotal()  ;
+		$grandTotalWithoutTax = $quote->getGrandTotal() - $totalTax;; 
+		/******/
+
+
+		
+		$discountAmount = $quote->getSubtotal() - $quote->getSubtotalWithDiscount();
+		$data['discount_amount'] = $this->priceHelper->currency($discountAmount, true, false); 
+		$data['grand_total_with_tax'] = $this->priceHelper->currency($grandTotalWithTax);
+		$data['grand_total_without_tax'] = $this->priceHelper->currency($grandTotalWithoutTax);
+		$data['my_tax'] = $this->priceHelper->currency($totalTax);
+
+		
 		return $data;
         
     }
--- view/frontend/web/js/view/checkout/minicart/subtotal/totals.js
+++ view/frontend/web/js/view/checkout/minicart/subtotal/totals.js
@@ -1,4 +1,3 @@
-console.log("My module file loaded");
 define([
     'ko',
     'uiComponent',
@@ -7,19 +6,25 @@ define([
     'use strict';
 
     return Component.extend({
-        displaySubtotal: ko.observable(true),
-
+       
         /**
          * @override
          */
         initialize: function () {
             this._super();
             this.cart = customerData.get('cart');
-            console.log("Discount my amount",this.cart().discount_amount);
         },
         getDiscountAmount: function() {
-        	this.cart = customerData.get('cart');
-        	return  "$" + parseFloat(this.cart().discount_amount).toFixed(2);
+            this.cart = customerData.get('cart');
+        	return this.cart().discount_amount
+        },
+        getGrandTotalWithTax: function() {
+            this.cart = customerData.get('cart');
+            return this.cart().grand_total_with_tax;
+        },
+        getGrandTotalWithoutTax :function() {
+            this.cart = customerData.get('cart');
+            return this.cart().grand_total_without_tax;
         }
     });
 });
